3a73ee726a1400656073efb1e898d4a24ec151a9
jakubasz <92277382+jakubasz@users.noreply.github.com>
2022-10-25 16:48:11 +0200

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,27 @@
+#include <iostream>
+using namespace std;
+
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+public:
+    Wektor2D() : mx(0.0), my(0.0) { cout << "d" << endl; }
+    Wektor2D(double x, double y) : mx(x), my(y) { cout << "p" << endl; }
+    Wektor2D(const Wektor2D& w) : mx(w.mx), my(w.my) { cout << "c" << endl; }
+
+    double getX() const { return mx;}
+    double getY() const { return my;}
+    void   setX(double x) { mx = x; }
+    void   setY(double y) { my = y; }
+
+    void copy(const Wektor2D& w); 
+    void print() { cout << mx << " " << my << endl; }
+   
+    Wektor2D operator+(const Wektor2D& w);
+
+private:
+    double mx;
+    double my;
+
 };
+
+double operator*(const Wektor2D l, const Wektor2D p);
